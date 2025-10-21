c4dba41074a67e09d6cca56952c5e214dd58c54c
Grant Merrill <jgrantfineart@gmail.com>
2025-10-21 09:04:11 -0500

    Initial commit from new X drive location

---
+++ images/Portraits/portrait_images.js
@@ -0,0 +1,9 @@
+const portraitImages = [
+    "leland.jpg",
+    "mothers_embrace.jpg",
+    "portrait_of_claire.jpg",
+    "portrait_of_kelly.jpg",
+    "sarah.jpg",
+    "self_portrait.jpg",
+    "the_banjo_player.jpg",
+];
